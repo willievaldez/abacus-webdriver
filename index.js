ff08fe5ae7c422b09bfe368feb2faa321e450df5
Guillermo Valdez <guillermo.valdez@everyonecounts.com>
2017-10-30 11:14:03 -0700

    added comments

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ dotenv.config({ path: `./env/${process.argv[2]}.env` });
 
 const params = {featureDir:'./features/', inclusiveTags: JSON.parse(process.env.INCLUSIVE_TAGS), exclusiveTags: JSON.parse(process.env.EXCLUSIVE_TAGS)};
 
+// filterScenarios parses all '*.feature' files and filters them by the given scenario tags
 filterScenarios(params).then((validScenarios)=>{
+  // TODO for this function: given the number of browser instances, divide the valid scenarios into packs and return an array of packs
   const divideScenarios = function(scenarios, instances) {
-    // TODO: Yeah...
     if(instances == 1)
       return [scenarios];
     else
@@ -16,6 +17,8 @@ filterScenarios(params).then((validScenarios)=>{
   };
 
   const scenarioPacks = divideScenarios(validScenarios, process.env.BROWSER_INSTANCES);
+
+  // for each browser instance, fork a runner and send along a pack of scenarios to execute
   scenarioPacks.forEach((pack)=>{
     // TODO might be easier to write pack to a json, and pass the filepath to the child process
     fork(`${__dirname}/runner.js`, [JSON.stringify(pack)], { env: process.env });
--- runner.js
+++ runner.js
@@ -1,6 +1,7 @@
 const gatherStepDefinitions = require(`${__dirname}/step-finder`);
 const gatherPageObjects = require(`${__dirname}/pageObject-finder`);
 
+// Global objects that will be seen by step definitions and page objects
 webdriver = require('selenium-webdriver'),
   By = webdriver.By,
   until = webdriver.until;
@@ -10,7 +11,8 @@ driver = new webdriver.Builder()
   .build();
 
 
-
+// gatherPageObjects: Look through all page object files, and construct a pageMap of the objects
+// gatherStepDefinitions: parse through all step definition files
 const promises = [gatherPageObjects('./page_objects/'),gatherStepDefinitions('./step_definitions/')];
 
 return Promise.all(promises).then((results) => {
@@ -18,8 +20,11 @@ return Promise.all(promises).then((results) => {
   const validScenarios = JSON.parse(process.argv[2]);
   const stepDefs = results[1];
 
+  //for each scenario in the pack, loop through each step and call the corresponding step definition function
   validScenarios.forEach((scenario) => {
     let stepNum = 0;
+
+    // recursive function that is called for each step in a scenario
     const callStep = function() {
       if (stepNum === scenario.steps.length) return;
       const currentStep = scenario.steps[stepNum];
