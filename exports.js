1f51108ff98340999926742b0f1cae82818ef869
Guillermo Valdez <guillermo.valdez@everyonecounts.com>
2017-10-30 17:43:04 -0700

    runner is now colored, and moved scripts into lib folder

--- exports.js
+++ exports.js
@@ -1,3 +1,4 @@
 module.exports = {
-  PageObject: require(`${__dirname}/page-object`)
+  PageObject: require(`${__dirname}/lib/page-object`),
+  RVG: require(`${__dirname}/lib/random-value-generator`)
 };
