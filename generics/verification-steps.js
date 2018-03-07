5d159bceb9b706c0162dac5ca2e3ffb946ee6a56
Jorge lopez <jorge.lopez@everyonecounts.com>
2018-03-07 12:13:47 -0800

    Rolling back changes from Step definition for url validation

--- generics/verification-steps.js
+++ generics/verification-steps.js
@@ -5,10 +5,10 @@ module.exports = ({Then}) => {
     Then(/^user is taken to the "([^"]*)" page$/, async (pageURI) => {
         global.pageID = pageURI;
         if (!pageMap[global.pageID]) throw new Error(`Page Object with name ${pageURI} is not defined`);
-        // const result = await driver.wait(until.urlMatches(pageMap[global.pageID].URL), process.env.CUCUMBER_REDIRECT_TIMEOUT * 1000, `URL never matched ${pageMap[global.pageID].URL}`)
-        // if (result !== true) {
-        //   return result;
-        // }
+        const result = await driver.wait(until.urlMatches(pageMap[global.pageID].URL), process.env.CUCUMBER_REDIRECT_TIMEOUT * 1000, `URL never matched ${pageMap[global.pageID].URL}`)
+        if (result !== true) {
+          return result;
+        }
 
         return null;
 
